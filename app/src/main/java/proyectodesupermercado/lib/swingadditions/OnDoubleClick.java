3cdc7aa1bc4e1eec4971a29a22b11d629747392d
Frairlyn Camilo Roque Suarez <frailin300@gmail.com>
2023-11-26 14:33:56 -0400

    feat(vista): Add new formulary

---
+++ app/src/main/java/proyectodesupermercado/lib/swingadditions/OnDoubleClick.java
@@ -0,0 +1,25 @@
+package proyectodesupermercado.lib.swingadditions;
+
+import javax.annotation.Nonnull;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.util.function.Consumer;
+
+/**
+ * Creates a mouse adapter that triggers each time there is double click
+ * The consumer function should interpret that it's dealing with the source of the click event.
+ */
+public class OnDoubleClick extends MouseAdapter {
+    private final Consumer<Object> doubleClickAction;
+
+    public OnDoubleClick(@Nonnull Consumer<Object> doubleClickAction) {
+        this.doubleClickAction = doubleClickAction;
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        if (e.getClickCount() > 1) {
+            doubleClickAction.accept(e.getSource());
+        }
+    }
+}
