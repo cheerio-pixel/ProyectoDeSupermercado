899576f8accdabd36186e0cdccb6f924a3f66c24
Frairlyn Camilo Roque Suarez <frailin300@gmail.com>
2023-11-29 19:58:50 -0400

    feature: Move Controller interfaces

--- app/src/main/java/proyectodesupermercado/controller/CreadorUsuario.java
+++ app/src/main/java/proyectodesupermercado/controller/CreadorUsuario.java
@@ -2,12 +2,14 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
  */
-package proyectodesupermercado.Controller;
+package proyectodesupermercado.controller;
+
+import proyectodesupermercado.controller.authentication.Usuario;
 
 /**
  *
  * @author DELL
  */
 public interface CreadorUsuario {
-    public Usuario registerUser(Usuario usuario);
+    Result<Usuario> registerUser(Usuario usuario);
 }
--- app/src/main/java/proyectodesupermercado/controller/SesionUsuario.java
+++ app/src/main/java/proyectodesupermercado/controller/SesionUsuario.java
@@ -2,13 +2,18 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
  */
-package proyectodesupermercado.Controller;
+package proyectodesupermercado.controller;
+
+import proyectodesupermercado.controller.authentication.Usuario;
 
 /**
  *
  * @author DELL
  */
 public interface SesionUsuario {
-    public Usuario getLoggedUser();
-    public Usuario logUser(Usuario usuario);
+    Usuario getLoggedUser();
+
+    Result<Usuario> logUser(String nombre, char[] contraseña);
+
+    void logOut();
 }
