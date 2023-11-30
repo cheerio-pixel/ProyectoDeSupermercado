e8976755054d105e52ae5021be8b3a805a39ffb7
Frairlyn Camilo Roque Suarez <frailin300@gmail.com>
2023-11-30 13:26:18 -0400

    refactor: Organize files into subpackages

---
+++ app/src/main/java/proyectodesupermercado/Vista/interfaces/SesionUsuario.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package proyectodesupermercado.Vista.interfaces;
+
+import proyectodesupermercado.controller.Result;
+import proyectodesupermercado.controller.authentication.Usuario;
+
+/**
+ *
+ * @author DELL
+ */
+public interface SesionUsuario {
+    Usuario getLoggedUser();
+
+    Result<Usuario> logUser(String nombre, char[] contraseña);
+
+    void logOut();
+}
