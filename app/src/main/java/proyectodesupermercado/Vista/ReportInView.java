816f532cccd00db18ae0cdec7c0b4672a2990e4c
Frairlyn Camilo Roque Suarez <frailin300@gmail.com>
2023-12-02 14:04:14 -0400

    Refactor(ReportInView): Change from JComponent to Component

--- app/src/main/java/proyectodesupermercado/Vista/ReportInView.java
+++ app/src/main/java/proyectodesupermercado/Vista/ReportInView.java
@@ -1,13 +1,13 @@
 package proyectodesupermercado.Vista;
 
-import javax.swing.JComponent;
 import javax.swing.JOptionPane;
+import java.awt.Component;
 
 public final class ReportInView {
     private ReportInView() {
     }
 
-    private static void messageDialog(JComponent parent, String msg, int type) {
+    private static void messageDialog(Component parent, String msg, int type) {
         JOptionPane.showMessageDialog(
                 parent,
                 msg, "",
@@ -15,21 +15,21 @@ public final class ReportInView {
         );
     }
 
-    public static void error(JComponent parent, String msg) {
+    public static void error(Component parent, String msg) {
         messageDialog(
                 parent, msg,
                 JOptionPane.ERROR_MESSAGE
         );
     }
 
-    public static void warning(JComponent parent, String msg) {
+    public static void warning(Component parent, String msg) {
         messageDialog(
                 parent, msg,
                 JOptionPane.WARNING_MESSAGE
         );
     }
 
-    public static void info(JComponent parent, String msg) {
+    public static void info(Component parent, String msg) {
         messageDialog(
                 parent, msg,
                 JOptionPane.INFORMATION_MESSAGE
