85c7f86e56aae72965406a30437322d100cc1a6c
Frairlyn Camilo Roque Suarez <frailin300@gmail.com>
2023-12-02 19:54:02 -0400

    Feature: Create temporal CRUD contract for Suplidor

---
+++ app/src/main/java/proyectodesupermercado/controller/dao/SuplidorDAO.java
@@ -0,0 +1,7 @@
+package proyectodesupermercado.controller.dao;
+
+import proyectodesupermercado.controller.crud.CRUD;
+import proyectodesupermercado.modelo.Suplidor;
+
+public interface SuplidorDAO extends CRUD<Suplidor> {
+}
