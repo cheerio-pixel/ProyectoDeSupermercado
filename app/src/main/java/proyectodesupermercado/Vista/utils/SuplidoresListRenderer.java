5d5d1906179b2ccd958496c3e2effe0d90d936d9
Frairlyn Camilo Roque Suarez <frailin300@gmail.com>
2023-12-03 15:44:23 -0400

    Fix(SuplidoresListRenderer): Fix slim null option

--- app/src/main/java/proyectodesupermercado/Vista/utils/SuplidoresListRenderer.java
+++ app/src/main/java/proyectodesupermercado/Vista/utils/SuplidoresListRenderer.java
@@ -8,10 +8,13 @@ import java.awt.Component;
 
 public class SuplidoresListRenderer extends DefaultListCellRenderer {
 
+    @Override
     public Component getListCellRendererComponent(JList<?> list, Object value, int index, boolean isSelected, boolean cellHasFocus) {
         Object res = value;
         if (value != null) {
             res = ((Suplidor) value).getNombre();
+        } else {
+            res = " ";
         }
         return super.getListCellRendererComponent(list, res, index, isSelected, cellHasFocus);
     }
