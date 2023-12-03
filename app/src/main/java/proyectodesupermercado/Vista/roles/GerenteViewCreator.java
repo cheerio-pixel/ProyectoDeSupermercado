48aa9a92ce7aa1261103c276069c6bd119792ebf
Frairlyn Camilo Roque Suarez <frailin300@gmail.com>
2023-12-03 15:05:50 -0400

    Feature: Propagate GerenteViewCreator dependency to constructor

--- app/src/main/java/proyectodesupermercado/Vista/roles/GerenteViewCreator.java
+++ app/src/main/java/proyectodesupermercado/Vista/roles/GerenteViewCreator.java
@@ -1,5 +1,6 @@
 package proyectodesupermercado.Vista.roles;
 
+import proyectodesupermercado.Vista.interfaces.ControlProductoRegistro;
 import proyectodesupermercado.Vista.paneles.CreacionProductos;
 import proyectodesupermercado.Vista.paneles.Creador_De_solicitud_De_Compra;
 import proyectodesupermercado.Vista.paneles.Gestor_De_Empresas;
@@ -8,12 +9,20 @@ import javax.swing.JPanel;
 
 public class GerenteViewCreator implements ViewCreator {
 
+    private final ControlProductoRegistro controlProductoRegistro;
+
+    public GerenteViewCreator(ControlProductoRegistro controlProductoRegistro) {
+        this.controlProductoRegistro = controlProductoRegistro;
+    }
+
     @Override
     public JPanel create() {
         GeneralGroup group = new GeneralGroup();
         group.addTab("Gestor de Empresas", new Gestor_De_Empresas());
         group.addTab("Manejador de compras", new Creador_De_solicitud_De_Compra());
-        group.addTab("Crear/Borrar productos", new CreacionProductos());
+        group.addTab("Crear/Borrar productos", new CreacionProductos(
+                controlProductoRegistro
+        ));
         return group;
     }
 }
