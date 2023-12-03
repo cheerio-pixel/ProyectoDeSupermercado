1d7fa41e6420ca58130e7c7313c1d64df71f7f5c
Frairlyn Camilo Roque Suarez <frailin300@gmail.com>
2023-12-03 15:02:07 -0400

    Feature: Make ProductoRegistroDAO Fallible

--- app/src/main/java/proyectodesupermercado/controller/dao/ProductoRegistroDAO.java
+++ app/src/main/java/proyectodesupermercado/controller/dao/ProductoRegistroDAO.java
@@ -1,11 +1,12 @@
 package proyectodesupermercado.controller.dao;
 
 import proyectodesupermercado.controller.crud.CRUD;
+import proyectodesupermercado.controller.crud.Fallible;
 import proyectodesupermercado.modelo.ProductoRegistro;
 import proyectodesupermercado.modelo.Suplidor;
 
 import java.util.List;
 
-public interface ProductoRegistroDAO extends CRUD<ProductoRegistro> {
+public interface ProductoRegistroDAO extends CRUD<ProductoRegistro>, Fallible {
     List<ProductoRegistro> searchByFields(String nombre, Suplidor suplidor);
 }
