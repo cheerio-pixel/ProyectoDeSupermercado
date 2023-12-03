d1fb52caf2d59093927b44d459dfe5994330df11
Frairlyn Camilo Roque Suarez <frailin300@gmail.com>
2023-12-03 15:03:29 -0400

    Feauture: Sort by name in DatabaseControlProductoRegistro view functions

--- app/src/main/java/proyectodesupermercado/controller/DatabaseControlUsuario.java
+++ app/src/main/java/proyectodesupermercado/controller/DatabaseControlUsuario.java
@@ -6,8 +6,9 @@ import proyectodesupermercado.controller.dao.UsuarioDAO;
 import proyectodesupermercado.lib.tableModel.ObjectTableModel;
 import proyectodesupermercado.modelo.Usuario;
 
-import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 public class DatabaseControlUsuario implements ControlEditarCrearUsuarios {
     private final UsuarioDAO usuarioDAO;
@@ -18,7 +19,13 @@ public class DatabaseControlUsuario implements ControlEditarCrearUsuarios {
 
     @Override
     public ObjectTableModel<Usuario> search(String prompt, boolean isSesionActiva, Rol rol) {
-        return new ObjectTableModel<>(Usuario.class, usuarioDAO.listByFields(rol, prompt, isSesionActiva));
+        return new ObjectTableModel<>(
+                Usuario.class,
+                usuarioDAO.listByFields(rol, prompt, isSesionActiva)
+                        .stream()
+                        .sorted(Comparator.comparing(Usuario::getNombre))
+                        .collect(Collectors.toList())
+        );
     }
 
     @Override
@@ -51,6 +58,12 @@ public class DatabaseControlUsuario implements ControlEditarCrearUsuarios {
 
     @Override
     public ObjectTableModel<Usuario> refresh() {
-        return new ObjectTableModel<>(Usuario.class, new ArrayList<>(usuarioDAO.listAll()));
+        return new ObjectTableModel<>(
+                Usuario.class,
+                usuarioDAO.listAll()
+                        .stream()
+                        .sorted(Comparator.comparing(Usuario::getNombre))
+                        .collect(Collectors.toList())
+        );
     }
 }
