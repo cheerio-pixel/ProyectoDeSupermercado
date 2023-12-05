8d31b71608211d4ee854ca10bb115692825d982a
IA <IA@DESKTOP-980VH5Q>
2023-12-05 17:46:04 -0500

    Bump dennis

---
+++ app/src/main/java/proyectodesupermercado/modelo/MiModeloDeTabla.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package proyectodesupermercado.modelo;
+
+import javax.swing.table.DefaultTableModel;
+
+
+/**
+ *
+ * @author IA
+ */
+public class MiModeloDeTabla extends DefaultTableModel {
+    public MiModeloDeTabla(Object[][] data, Object[] columnNames) {
+        super(data, columnNames);
+    }
+}
